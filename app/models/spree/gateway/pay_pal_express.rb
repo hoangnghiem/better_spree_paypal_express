db13e7094b485a07596a3318de52c64fc2963376
hoangnghiem <hoangnghiem1711@gmail.com>
2016-03-09 16:26:42 +0700

    Add cancel payment

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -34,11 +34,74 @@ module Spree
       'paypal'
     end
 
+    def empty_success
+      Class.new do
+        def success?; true; end
+        def authorization; nil; end
+      end.new
+    end
+    
+
+    def void(response_code, gateway_options={})
+      Rails.logger.info"Voiding transction ID #{response_code}"
+      payment = Spree::Payment.find_by_response_code(response_code)
+      Rails.logger.info"Voiding pyament: #{payment.inspect}"
+      amount = payment.credit_allowed
+
+      #in case a partially refunded payment gets cancelled/voided, we don't want to act on the refunded payments
+      if amount.to_f > 0
+
+        # Process the refund
+        refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
+
+        refund_transaction = provider.build_refund_transaction({
+          :TransactionID => payment.source.transaction_id,
+          :RefundType => refund_type,
+          :Amount => {
+            :currencyID => payment.currency,
+            :value => amount },
+          :RefundSource => "any" })
+        
+        refund_transaction_response = provider.refund_transaction(refund_transaction)
+
+        Rails.logger.info "Refund transaction response from paypal #{refund_transaction_response.inspect}"
+      
+        if refund_transaction_response.success?
+          payment.source.update_attributes({
+            :refunded_at => Time.now,
+            :refund_transaction_id => refund_transaction_response.RefundTransactionID,
+            :state => "refunded",
+            :refund_type => refund_type
+          })
+          empty_success
+        else
+          class << refund_transaction_response
+            def to_s
+              errors.map(&:long_message).join(" ")
+            end
+          end
+          refund_transaction_response
+        end
+      end
+
+      empty_success
+    end
+
+    #cancellations also work for a partially refunded payment
+    def cancel(response_code)
+      void(response_code, {})
+    end
+
     def purchase(amount, express_checkout, gateway_options={})
+      Rails.logger.info "Express purchase"
+      Rails.logger.info "Gateway optiosn #{gateway_options}"
+
       pp_details_request = provider.build_get_express_checkout_details({
         :Token => express_checkout.token
       })
       pp_details_response = provider.get_express_checkout_details(pp_details_request)
+
+      Rails.logger.info "Paypal response #{pp_details_response.inspect}"
 
       pp_request = provider.build_do_express_checkout_payment({
         :DoExpressCheckoutPaymentRequestDetails => {
@@ -50,16 +113,23 @@ module Spree
       })
 
       pp_response = provider.do_express_checkout_payment(pp_request)
+      Rails.logger.info "Paypal do express response #{pp_response.inspect}"
       if pp_response.success?
         # We need to store the transaction id for the future.
         # This is mainly so we can use it later on to refund the payment if the user wishes.
         transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
+        Rails.logger.info "Paypal transaction ID #{transaction_id}"
         express_checkout.update_column(:transaction_id, transaction_id)
-        # This is rather hackish, required for payment/processing handle_response code.
-        Class.new do
-          def success?; true; end
-          def authorization; nil; end
-        end.new
+
+        #"order_id: The Order’s number attribute, plus the identifier for each payment, generated when the payment is first created"
+        payment = Spree::Payment.find_by_number(gateway_options[:order_id].split('-').last)
+        Rails.logger.info "Found payment #{payment.inspect}"
+        payment.update_attribute(:response_code, transaction_id)
+
+        Rails.logger.info "Save transaction ID to payment"
+        Rails.logger.info "Payment #{payment.inspect}"
+
+        empty_success
       else
         class << pp_response
           def to_s
